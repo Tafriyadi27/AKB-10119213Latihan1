7e5efe280e927c404bfbbfdc410540011787daee
Tafriyadi27 <87406088+Tafriyadi27@users.noreply.github.com>
2022-04-16 23:02:20 +0700

    LittleRefactor

--- app/src/main/java/com/skullbreker/a10119213latihan1/MainActivity.java
+++ app/src/main/java/com/skullbreker/a10119213latihan1/MainActivity.java
@@ -3,7 +3,10 @@ package com.skullbreker.a10119213latihan1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+// Nama                 : Tri Tafriyadi
+// NIM                  : 10119213
+// Kelas                : IF-6
+// Tanggal Pengerjaan   : 15-04-2022
 public class MainActivity extends AppCompatActivity {
 
     @Override
